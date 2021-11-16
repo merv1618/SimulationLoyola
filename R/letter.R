9e1d8f38e5974d7c1eaf0e44024ce6eef03cfa1d
merv1618 <43761721+merv1618@users.noreply.github.com>
2021-11-15 19:42:07 -0600

    Add files via upload
    11/15 Sim test package homework upload

---
+++ R/letter.R
@@ -0,0 +1,6 @@
+letter <-
+function(){
+  alphabet <- c("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
+  x <- sample(1:26, 1)
+  return(alphabet[x])
+}
